12cd3c9b492c500eb7fdffe484ee4e59f02d9078
yhsj0919 <1130402124@qq.com>
2021-06-09 18:06:48 +0800

    mg搜索页面

--- example/lib/search/search_page.dart
+++ example/lib/search/search_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kmusic_api_example/search/search_controller.dart';
+import 'package:kmusic_api_example/widget/blur_widget.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key key}) : super(key: key);
@@ -14,85 +15,117 @@ class _SearchPageState extends State<SearchPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        titleSpacing: 0,
-        title: TextField(
-          textInputAction: TextInputAction.search,
-          onSubmitted: (value) {
-            print("$value");
-          },
-          decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: "请输入关键字",
-            prefixIcon: Hero(tag: "tag", child: Icon(Icons.search)),
-            suffixIcon: Icon(Icons.clear),
+    return Container(
+      color: Colors.white,
+      child: BlurWidget(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            titleSpacing: 0,
+            title: TextField(
+              textInputAction: TextInputAction.search,
+              onSubmitted: (value) {
+                print("$value");
+              },
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "请输入关键字",
+                prefixIcon: Hero(tag: "tag", child: Icon(Icons.search)),
+                suffixIcon: Icon(Icons.clear),
+              ),
+            ),
+          ),
+          body: Obx(
+            () => SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                children: [
+                  discovery(),
+                  hotWord(),
+                ],
+              ),
+            ),
           ),
         ),
       ),
-      body: Obx(
-        () => SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "搜索发现",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ).paddingSymmetric(horizontal: 16, vertical: 8),
-              Wrap(
-                spacing: 10,
-                runSpacing: 10,
-                children: searchController.discovery
-                    .map((element) => MaterialButton(
-                          onPressed: () {},
-                          child: Text(
-                            element['word'],
-                            style: TextStyle(fontSize: 13),
-                          ),
-                          elevation: 0,
-                          hoverElevation: 0,
-                          highlightElevation: 0,
-                          focusElevation: 0,
-                          color: Color(0xffeeeeee),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                          ),
-                        ))
-                    .toList(),
-              ).marginOnly(left: 16, right: 16, bottom: 8),
-              Text(
-                "实时热搜",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                ),
-              ).paddingSymmetric(horizontal: 16, vertical: 8),
-              ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: searchController.hotwords.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: () {},
-                    title: Text(
-                      "${index + 1}   " + searchController.hotwords[index]['word'],
-                      style: TextStyle(fontSize: 14, color: index < 3 ? Colors.black : Color(0xff666666)),
-                    ),
-                    trailing: Text(
-                      searchController.hotwords[index]['note'],
-                      style: TextStyle(fontSize: 14, color: Color(0xff999999)),
-                    ),
-                  );
-                },
-              ),
-            ],
+    );
+  }
+
+  ///热门搜索
+  Widget hotWord() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "实时热搜",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
           ),
+        ).paddingSymmetric(horizontal: 16, vertical: 8),
+        ListView.builder(
+          physics: const NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: searchController.hotwords.length,
+          itemBuilder: hotWordItem,
         ),
+      ],
+    );
+  }
+
+  //热门搜索item
+  Widget hotWordItem(context, index) {
+    return ListTile(
+      onTap: () {},
+      title: Text(
+        "${index + 1}   " + searchController.hotwords[index]['word'],
+        style: TextStyle(fontSize: 14, color: index < 3 ? Colors.black : Color(0xff666666)),
+      ),
+      trailing: Text(
+        searchController.hotwords[index]['note'],
+        style: TextStyle(fontSize: 14, color: Color(0xff999999)),
+      ),
+    );
+  }
+
+  ///发现
+  Widget discovery() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "搜索发现",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ).paddingSymmetric(horizontal: 16, vertical: 8),
+        Wrap(
+          spacing: 10,
+          runSpacing: 10,
+          children: searchController.discovery.map(discoveryButton).toList(),
+        ).marginOnly(left: 16, right: 16, bottom: 8),
+      ],
+    );
+  }
+
+  ///发现内部按钮
+  Widget discoveryButton(data) {
+    return MaterialButton(
+      onPressed: () {},
+      child: Text(
+        data['word'],
+        style: TextStyle(fontSize: 13),
+      ),
+      elevation: 0,
+      hoverElevation: 0,
+      highlightElevation: 0,
+      focusElevation: 0,
+      color: Color(0xffeeeeee),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
     );
   }
